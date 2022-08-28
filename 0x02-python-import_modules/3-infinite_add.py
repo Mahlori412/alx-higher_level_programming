56505ff2217d5549ac6e8c010c21bf94400edd71
Mahlori412 <manganyimahlori6@gmail.com>
2022-08-28 19:52:28 +0200

    still fixing

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+from sys import argv
+if __name__ == '__main__':
+    result = 0
+    for i in argv[1:]:
+        result += int(i)
+    print(result)
