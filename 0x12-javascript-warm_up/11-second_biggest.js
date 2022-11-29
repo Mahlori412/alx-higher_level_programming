d88c62f90cbd9dbc13c7636bca43c698ffbb6085
Mahlori412 <manganyimahlori6@gmail.com>
2022-11-29 20:58:22 +0200

    dddxx

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,26 +1,9 @@
 #!/usr/bin/node
-
-const len = process.argv.length;
-const nums = process.argv.slice(2).map(function (n) {
-  return parseInt(n);
-});
-const max = Math.max.apply(Math, nums);
-const min = Math.min.apply(Math, nums);
-
-if (len > 3) {
-  let i = 0;
-  let n = 0;
-  let secBig = min;
-
-  for (; i < len; ++i) {
-    n = nums[i];
-
-    if (n > secBig && n < max) {
-      secBig = n;
-    }
-  }
-
-  console.log(secBig);
-} else {
+if (process.argv.length <= 3) {
   console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
