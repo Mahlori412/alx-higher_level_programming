d3fc3dc292d6eb6d2513529d0606a994d74f0bf3
Mahlori412 <manganyimahlori6@gmail.com>
2023-01-19 06:37:14 +0200

    try

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+const path = require('path');
+
+const filePath = path.resolve(process.argv[2]);
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
 });
